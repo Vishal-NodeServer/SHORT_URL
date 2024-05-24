a60db5d84e770c1d2573c6b4c8f7504ef3b279b6
vishal-NodeServer <vishal123suthar2@gmail.com>
2024-05-25 01:09:58 +0530

    auth via Header

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,42 +1,70 @@
 const { getUser } = require("../service/auth");
 
-async function restrictToLoggedinUserOnly(req, res, next) {
-  const userUid = req.headers["authorization"];
-  console.log(req.headers);
-  if (!userUid) return res.redirect("/login");
+function checkForAuthentication(req , res , next) {
+      
+  const authorizationHeaderValue = req.headers["authorization"];
+    req.user = null;
+
+  if(!authorizationHeaderValue || !authorizationHeaderValue.startsWith('Bearer'))
+    return next();
+
+  const token = authorizationHeaderValue.split("Bearer ")[1];
+
+  const user = getUser(token);
+
+  req.user = user;
+  return next();
+  
+
+}
+
+function restrictTo(roles){
+      return function(req , res, next){
+        if(!req.user)  return res.redirect("/login");
+        
+      };
+}
+
+
+
+// async function restrictToLoggedinUserOnly(req, res, next) {
+//   const userUid = req.headers["authorization"];
+//   console.log(req.headers);
+//   if (!userUid) return res.redirect("/login");
        
-  const token = userUid.startsWith("Bearer ") ? userUid.split("Bearer ")[1] : null;
-  if (!token) return res.redirect("/login");
+//   const token = userUid.startsWith("Bearer ") ? userUid.split("Bearer ")[1] : null;
+//   if (!token) return res.redirect("/login");
 
-  const user = await getUser(token);
+//   const user = await getUser(token);
   
-  if (!user) return res.redirect("/login");
+//   if (!user) return res.redirect("/login");
 
-  req.user = user;
-  next();
-}
+//   req.user = user;
+//   next();
+// }
 
-async function checkAuth(req, res, next) {
-  const userUid = req.headers["authorization"];
+// async function checkAuth(req, res, next) {
+//   const userUid = req.headers["authorization"];
 
   
-  if (!userUid) {
-    req.user = null;
-    return next();
-  }
+//   if (!userUid) {
+//     req.user = null;
+//     return next();
+//   }
 
-  const token = userUid.startsWith("Bearer ") ? userUid.split("Bearer ")[1] : null;
-  if (!token) {
-    req.user = null;
-    return next();
-  }
+//   const token = userUid.startsWith("Bearer ") ? userUid.split("Bearer ")[1] : null;
+//   if (!token) {
+//     req.user = null;
+//     return next();
+//   }
 
-  const user = await getUser(token);
-  req.user = user;
-  next();
-}
+//   const user = await getUser(token);
+//   req.user = user;
+//   next();
+// }
 
 module.exports = {
-  restrictToLoggedinUserOnly,
-  checkAuth,
+  // restrictToLoggedinUserOnly,
+  // checkAuth,
+  checkForAuthentication,
 };
